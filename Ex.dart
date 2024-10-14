6a3eeb184bce80e34263806fe9fec7429a4cb06e
LisbethMendoza <lisbethango223019@gmail.com>
2024-10-14 18:44:15 -0400

    feat: ex4 and 5

--- Ex.dart
+++ Ex.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 //Ejercicio 1: Clases y Objetos
 //Define una clase Persona con atributos, nombre y edad. Crea una instancia de esta clase e imprime sus valores.
 //Agrega un método para mostrar los detalles de la persona.
@@ -5,12 +7,18 @@
 void main() {
   print('--Ejercicio 1--');
   ex1();
-
+  print('');
   print('--Ejercicio 2--');
   ex2();
-
-  print('--Ejercicio 2--');
+  print('');
+  print('--Ejercicio 3--');
   ex3();
+  print('');
+  print('--Ejercicio 4--');
+  //ex4();
+  print('');
+  print('--Ejercicio 5--');
+  ex5();
 }
 
 //-------------Ejercicio 1-------------------
@@ -79,5 +87,92 @@ void ex3() {
 }
 
 //-------------Ejercicio 4-------------------
+//Extensiones y Enumeraciones
+//Crea una enumeración DiaSemana con los días de la semana.
+//Define una extensión sobre int que devuelva true si el número es par. Úsalo para verificar si un número ingresado por el usuario es par.
+
+enum DiaSemana {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  TRUSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+
+extension operacion on int {
+  bool esPar() {
+    return this % 2 == 0;
+  }
+}
+
+void ex4() {
+  print('Ingrese un número entero del 1 al 7:');
+  String? input = stdin.readLineSync();
+
+  if (input != null) {
+    int? numero = int.tryParse(input);
+
+    if (numero != null && numero >= 1 && numero <= 7) {
+      if (numero.esPar()) {
+        print('El número $numero es par.');
+      } else {
+        print('El número $numero es impar.');
+      }
+
+      // Obtenemos el día correspondiente de la enumeración
+      DiaSemana dia = DiaSemana.values[numero - 1];
+      print('El día correspondiente es: ${dia.toString().split('.').last}');
+    } else {
+      print('Por favor, ingrese un número válido del 1 al 7.');
+    }
+  }
+}
 
 //-------------Ejercicio 5-------------------
+//Crea un mixin Nadador con un método nadar(). Agrégalo a una clase Delfín que herede de la clase abstracta Animal.
+//Define un Factory constructor en una clase Vehículo que devuelva una instancia de diferentes tipos de vehículos dependiendo del tipo ingresado (e.g., "auto", "moto").
+
+abstract class Vehiculo {
+  void encender();
+
+  factory Vehiculo(String tipo) {
+    if (tipo == 'auto') {
+      return Auto();
+    } else if (tipo == 'moto') {
+      return Moto();
+    } else {
+      throw 'Tipo de vehículo no reconocido: $tipo';
+    }
+  }
+}
+
+class Auto implements Vehiculo {
+  @override
+  void encender() {
+    print("El auto está encendido.");
+  }
+}
+
+class Moto implements Vehiculo {
+  @override
+  void encender() {
+    print("La moto está encendida.");
+  }
+}
+
+void ex5() {
+  Vehiculo vehiculo1 = Vehiculo('auto');
+  vehiculo1.encender();
+
+  Vehiculo vehiculo2 = Vehiculo('moto');
+  vehiculo2.encender();
+
+  try {
+    Vehiculo vehiculo3 = Vehiculo('camion');
+    vehiculo3.encender();
+  } catch (e) {
+    print(e);
+  }
+}
